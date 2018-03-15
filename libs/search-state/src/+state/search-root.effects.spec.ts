9753c4f6ff918ec89973549c1af34b9229107101
Balazs Korossy-Khayll <khayll@192.168.0.17>
2018-03-15 15:15:25 +0100

    fixed effects test

--- libs/search-state/src/+state/search-root.effects.spec.ts
+++ libs/search-state/src/+state/search-root.effects.spec.ts
@@ -5,12 +5,16 @@ import {DataPersistence} from '@nrwl/nx';
 import {hot} from '@nrwl/nx/testing';
 import {SearchRootEffects} from './search-root.effects';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
-import {searchRootReducer} from "@repoinfo/search-state";
 import {Observable} from "rxjs/Observable";
+import 'rxjs/add/observable/of';
+import {HttpClient} from "@angular/common/http";
+import {delay} from "rxjs/operators/delay";
 
 describe('SearchRootEffects', () => {
   let actions: Observable<any>;
   let effects: SearchRootEffects;
+
+  const testPayload = {total_count: 1, items:[], incomplete_results: false};
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -26,13 +30,18 @@ describe('SearchRootEffects', () => {
     });
 
     effects = TestBed.get(SearchRootEffects);
+    const httpMock = TestBed.get(HttpClient);
+
+    spyOn(httpMock, 'get').and.returnValue(
+      Observable.of(testPayload)
+    );
   });
 
   describe('someEffect', () => {
     it('should work', () => {
       actions = hot('-a-|', {a: {type: 'SEARCH_LOAD_DATA', payload: 'testQuery'}});
       expect(effects.loadData).toBeObservable(
-        hot('-a-|', {a: {type: 'SEARCH_DATA_LOADED', payload: {}}})
+        hot('-a-|', {a: {type: 'SEARCH_DATA_LOADED', payload: {query: 'testQuery', ...testPayload}}})
       );
     });
     it('should work for empty searches', () => {
